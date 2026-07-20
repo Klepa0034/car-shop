17740f36c23a20bad28f9ee20709daf1a3f87aee
Klepa0034 <sorokinga04@mail.ru>
2026-07-20 12:11:03 +0400

    CS-5 Исправил ошибки

--- src/main/java/org/example/carshop/car/feature/delete/controller/CarDeleteController.java
+++ src/main/java/org/example/carshop/car/feature/delete/controller/CarDeleteController.java
@@ -1,21 +1,23 @@
 package org.example.carshop.car.feature.delete.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.carshop.car.feature.delete.model.CarDeleteResponse;
 import org.example.carshop.car.feature.delete.service.CarDeleteService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/cars")
 @RequiredArgsConstructor
+@RequestMapping("/api/cars")
 public class CarDeleteController {
     private final CarDeleteService carDeleteService;
 
     @DeleteMapping("/{id}")
-    void delete (@PathVariable Long id){
-        carDeleteService.handle(id);
+    public ResponseEntity<Void> delete (@PathVariable Long id){
+         carDeleteService.handle(id);
+         return ResponseEntity.noContent().build();
     }
 }
-
--- src/main/java/org/example/carshop/car/feature/delete/service/CarDeleteServiceImpl.java
+++ src/main/java/org/example/carshop/car/feature/delete/service/CarDeleteServiceImpl.java
@@ -2,6 +2,7 @@ package org.example.carshop.car.feature.delete.service;
 
 import lombok.AllArgsConstructor;
 import org.example.carshop.car.feature.delete.repository.CarDeleteRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
